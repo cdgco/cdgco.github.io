cb5aebd416bbf27b4376f3d5a82349abd3efdbd4
CDG <cdgco@users.noreply.github.com>
2017-01-15 17:51:51 -0800

    Update g2.sh

--- g2.sh
+++ g2.sh
@@ -33,7 +33,7 @@ sleep .8
 echo "Connecting to GooGifs Repository ..."
 sleep .2
 git clone https://github.com/cdgco/GooGifs.git
-cd GooGifs || exit
+cd GooGifss || exit
 rm composer.json
 rm appveyor.yml
 rm README.md
